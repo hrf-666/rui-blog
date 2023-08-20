dc7a03d3f0fe83d9d8363db1528bdb29e7d5abe9
huang-rui <3398775460@qq.com>
2023-08-20 13:17:47 +0800

    自动部署

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,48 +1,47 @@
-import { defineUserConfig } from "vuepress";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
-import { gitPlugin } from "@vuepress/plugin-git";
+import {defineUserConfig} from "vuepress";
+import {searchProPlugin} from "vuepress-plugin-search-pro";
+import {gitPlugin} from "@vuepress/plugin-git";
 
 import theme from "./theme.js";
 
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
 export default defineUserConfig({
-  base: "/ruiblogs/",
+    base: "/ruiblogs/",
 
-  lang: "zh-CN",
-  title: "Rui的博客 | Rui Blog",
-  description: "技术分享，资源分享",
+    lang: "zh-CN",
+    title: "Rui的博客 | Rui Blog",
+    description: "技术分享，资源分享",
 
-  theme,
+    theme,
 
-  // Enable it with pwa
-  shouldPrefetch: false,
-  plugins: [
-    //  搜索功能
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter as any,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag as any,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
-    gitPlugin({
-      // 配置项
-      createdTime: true,
-      updatedTime: true,
-      contributors: true,
-
-    }),
-
-
-
-
-  ],
+    // Enable it with pwa
+    shouldPrefetch: false,
+    plugins: [
+        //  搜索功能
+        searchProPlugin({
+            // 索引全部内容
+            indexContent: true,
+            // 为分类和标签添加索引
+            customFields: [
+                {
+                    getter: (page) => page.frontmatter as any,
+                    formatter: "分类：$content",
+                },
+                {
+                    getter: (page) => page.frontmatter.tag as any,
+                    formatter: "标签：$content",
+                },
+            ],
+        }),
+        gitPlugin({
+            // 配置项
+            createdTime: true,
+            updatedTime: true,
+            contributors: true,
+        }),
+    ],
 
 });
