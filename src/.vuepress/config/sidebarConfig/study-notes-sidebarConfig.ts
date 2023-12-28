661082180665648c1833adbb28ddea2d610d5b1d
hrf-666 <3398775460@qq.com>
2023-12-28 14:48:52 +0800

    修改

--- src/.vuepress/config/sidebarConfig/study-notes-sidebarConfig.ts
+++ src/.vuepress/config/sidebarConfig/study-notes-sidebarConfig.ts
@@ -1,30 +1,30 @@
 export const study_notes_sidebarConfig = [
+    // {children: "structure",}
         {
             text: '后端',   // 必要的
             link: '/study-notes/back-end/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
             initialOpenGroupIndex: -1, // 可选的, 默认值是 0
             collapsible: false, // 可选的, 默认值是 true,
-            children: "structure",
-            // children: [
-            //     {
-            //         text: 'Java',   // 必要的
-            //         link: '/study-notes/back-end/java/',
-            //         children: [
-            //             {
-            //                 text: 'Java基础',   // 必要的
-            //                 link: '/study-notes/back-end/java/basic/'
-            //             },
-            //             {
-            //                 text: 'JavaSE',   // 必要的
-            //                 link: '/study-notes/back-end/java/javase/',
-            //             }
-            //         ]
-            //     },
-            //     {
-            //         text: 'Python',   // 必要的
-            //         link: '/study-notes/back-end/python/'
-            //     }
-            // ]
+            children: [
+                {
+                    text: 'Java',   // 必要的
+                    link: '/study-notes/back-end/java/',
+                    children: [
+                        {
+                            text: 'Java基础',   // 必要的
+                            link: '/study-notes/back-end/java/basic/'
+                        },
+                        {
+                            text: 'JavaSE',   // 必要的
+                            link: '/study-notes/back-end/java/javase/',
+                        }
+                    ]
+                },
+                {
+                    text: 'Python',   // 必要的
+                    link: '/study-notes/back-end/python/'
+                }
+            ]
         },
         {
             text: '前端',
